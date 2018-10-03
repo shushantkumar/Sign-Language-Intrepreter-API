933a63923c12bbd45c4fd7f8f88b90d617bdfd69
Shushant Kumar <shushantkmr2@gmail.com>
2018-10-04 00:39:43 +0530

    removed packages

--- restimage/webapp/view.py
+++ restimage/webapp/view.py
@@ -1,4 +1,3 @@
-#from django.conf.settings import PROJECT_ROOT
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
